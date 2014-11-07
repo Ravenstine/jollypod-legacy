36ce15e50a47a23bc91f20db675fe8f4405a3673
benjamin <benjamin@benjamin.(none)>
2014-11-07 14:13:56 -0800

    Add api request for playlist data.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,6 +2,8 @@ require 'net/https'
 
 class Playlist
 
+  attr_accessor :title, :description, :author, :link, :image, :pub_date, :language, :copyright, :items
+
   def initialize id
     @id = id
     @pages = []
@@ -31,6 +33,20 @@ class Playlist
     uri = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=#{@id}&key=#{Rails.application.secrets.youtube_api_key}"
     uri += "&pageToken=#{next_page_token}" if next_page_token
     JSON.parse URI.parse(uri).read
+  end
+
+  def download_info
+    uri = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=#{@id}&key=#{Rails.application.secrets.youtube_api_key}"
+    if response = (JSON.parse URI.parse(uri).read)
+      @title = response["items"][0]["snippet"]["title"]
+      @description = response["items"][0]["snippet"]["description"]
+      @author = response["items"][0]["snippet"]["channelTitle"]
+      @link = "https://youtube.com/user/" + response["items"][0]["snippet"]["channelTitle"]
+      @image = response["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+      @pub_date = response["items"][0]["snippet"]["publishedAt"]
+    else
+      nil
+    end
   end
 
 end
